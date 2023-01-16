d1ae7500ece1dc1124ce7892c63fd3534fe994cc
Oleg Petrov <petrov.oleg.s@gmail.com>
2023-01-16 08:29:42 +0000

    🚀 Build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,2 @@
-/**
- * Orient image and execute callback with base64 param
- *
- * @param {File} file
- * @param {Function} callback — receive a base64 image param
- */
 declare const resetOrientation: (file: File, callback: (b64: string) => void) => void;
 export default resetOrientation;
--- dist/index.js
+++ dist/index.js
@@ -1,32 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * Get orientation code from image file
- *
- * @param {File} file
- * @param {Function} callback — receive a code of orientation
- */
-var getOrientation = function (file, cb) {
-    var reader = new FileReader();
-    reader.onload = function () {
-        var view = new DataView(reader.result);
+const getOrientation = (file, cb) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+        const view = new DataView(reader.result);
         if (view.getUint16(0, false) !== 0xFFD8) {
             return cb(-2);
         }
-        var length = view.byteLength;
-        var offset = 2;
+        const length = view.byteLength;
+        let offset = 2;
         while (offset < length) {
-            var marker = view.getUint16(offset, false);
+            const marker = view.getUint16(offset, false);
             offset += 2;
             if (marker === 0xFFE1) {
                 if (view.getUint32(offset += 2, false) != 0x45786966) {
                     return cb(-1);
                 }
-                var little = view.getUint16(offset += 6, false) === 0x4949;
+                const little = view.getUint16(offset += 6, false) === 0x4949;
                 offset += view.getUint32(offset + 4, little);
-                var tags = view.getUint16(offset, little);
+                const tags = view.getUint16(offset, little);
                 offset += 2;
-                for (var i = 0; i < tags; i++) {
+                for (let i = 0; i < tags; i++) {
                     if (view.getUint16(offset + (i * 12), little) == 0x0112) {
                         return cb(view.getUint16(offset + (i * 12) + 8, little));
                     }
@@ -43,23 +37,16 @@ var getOrientation = function (file, cb) {
     };
     reader.readAsArrayBuffer(file.slice(0, 64 * 1024));
 };
-/**
- * Orient image and execute callback with base64 param
- *
- * @param {File} file
- * @param {Function} callback — receive a base64 image param
- */
-var resetOrientation = function (file, callback) {
-    var img = new Image();
-    var reader = new FileReader();
+const resetOrientation = (file, callback) => {
+    const img = new Image();
+    const reader = new FileReader();
     reader.readAsDataURL(file);
     img.onload = function () {
-        var width = img.width;
-        var height = img.height;
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
-        // set proper canvas dimensions before transform & export
-        getOrientation(file, function (srcOrientation) {
+        const width = img.width;
+        const height = img.height;
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        getOrientation(file, (srcOrientation) => {
             if (srcOrientation > 4 && srcOrientation < 9) {
                 canvas.width = height;
                 canvas.height = width;
@@ -68,7 +55,6 @@ var resetOrientation = function (file, callback) {
                 canvas.width = width;
                 canvas.height = height;
             }
-            // transform context before drawing image
             switch (srcOrientation) {
                 case 2:
                     ctx && ctx.transform(-1, 0, 0, 1, width, 0);
@@ -93,15 +79,13 @@ var resetOrientation = function (file, callback) {
                     break;
                 default: break;
             }
-            // draw image
             if (ctx) {
                 ctx.drawImage(img, 0, 0);
             }
-            // export base64
             callback(canvas.toDataURL());
         });
     };
-    reader.onloadend = function () {
+    reader.onloadend = () => {
         img.src = reader.result;
     };
 };
