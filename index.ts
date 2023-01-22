9c30d8279d1aae5c9ae2f0164aaa0aa396dd8a00
Oleg Petrov <petrov.oleg.s@gmail.com>
2023-01-22 22:01:55 +0000

    📝 Fix JSDoc param

--- index.ts
+++ index.ts
@@ -2,36 +2,36 @@
  * Get orientation code from image file
  *
  * @param {File} file
- * @param {Function} callback — receive a code of orientation
+ * @param {Function} cb — receive a code of orientation
  */
 const getOrientation = (file: File, cb: (orintationCode: number) => void) => {
     const reader = new FileReader()
-  
+
     reader.onload = () => {
         const view = new DataView(reader.result as ArrayBuffer)
-  
+
         if (view.getUint16(0, false) !== 0xFFD8) {
             return cb(-2)
         }
-    
+
         const length = view.byteLength
         let offset = 2
-    
+
         while (offset < length) {
             const marker = view.getUint16(offset, false)
             offset += 2
-    
+
             if (marker === 0xFFE1) {
-                
+
                 if (view.getUint32(offset += 2, false) != 0x45786966) {
                     return cb(-1)
                 }
-            
+
                 const little = view.getUint16(offset += 6, false) === 0x4949
                 offset += view.getUint32(offset + 4, little)
                 const tags = view.getUint16(offset, little)
                 offset += 2
-    
+
                 for (let i = 0; i < tags; i++) {
                     if (view.getUint16(offset + (i * 12), little) == 0x0112) {
                         return cb(view.getUint16(offset + (i * 12) + 8, little))
@@ -45,7 +45,7 @@ const getOrientation = (file: File, cb: (orintationCode: number) => void) => {
       }
       return cb(-1)
     }
-  
+
     reader.readAsArrayBuffer(file.slice(0, 64 * 1024))
 }
 
@@ -66,7 +66,7 @@ const resetOrientation = (file: File, callback: (b64: string) => void) => {
         const height = img.height
         const canvas = document.createElement('canvas')
         const ctx = canvas.getContext('2d')
-  
+
         // set proper canvas dimensions before transform & export
         getOrientation(file, (srcOrientation: number) => {
             if (srcOrientation > 4 && srcOrientation < 9) {
@@ -76,7 +76,7 @@ const resetOrientation = (file: File, callback: (b64: string) => void) => {
                 canvas.width = width
                 canvas.height = height
             }
-    
+
             // transform context before drawing image
             switch (srcOrientation) {
                 case 2: ctx && ctx.transform(-1, 0, 0, 1, width, 0); break
@@ -88,17 +88,17 @@ const resetOrientation = (file: File, callback: (b64: string) => void) => {
                 case 8: ctx && ctx.transform(0, -1, 1, 0, 0, width); break
                 default: break
             }
-    
+
             // draw image
             if (ctx) {
                 ctx.drawImage(img, 0, 0)
             }
-    
+
             // export base64
             callback(canvas.toDataURL())
         })
     }
-    
+
     reader.onloadend = () => {
         img.src = reader.result as string
     }
